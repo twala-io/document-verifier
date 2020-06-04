0a488c2967aa46d516c70a4b686e5d36ca7cd82f
Alexander Paul P. Quinit <paulquinit@gmail.com>
2020-06-04 19:26:47 +0800

    Update verification display

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Command-line interface for verifying Twala documents.
 
-![Node.js CI](https://github.com/twala-io/twala-verify/workflows/Node.js%20CI/badge.svg)
 [![Version](https://img.shields.io/npm/v/@twala-io/twala-verify.svg)](https://npmjs.org/package/@twala-io/twala-verify)
 [![Downloads/week](https://img.shields.io/npm/dw/@twala-io/twala-verify.svg)](https://npmjs.org/package/@twala-io/twala-verify)
 [![License](https://img.shields.io/npm/l/@twala-io/twala-verify.svg)](https://github.com/twala-io/twala-verify/blob/master/package.json)
+
+## Requirements
+
+- Node.js >= 8.0.0
+- npm >= 3.0.0
 
 # Installation
 
@@ -21,4 +25,4 @@ $ twala-verify (-v|--version)
 $ twala-verify (-h|--help)
 ```
 
-**Note:**  You will be asked to provide three parameters, an Ethereum node url, the proof file path, and the document file path. For the Ethereum node url, you can provision your own geth instance on your machine, or you can use a third-party node-as-a-service provider such as [Infura](https://infura.io).
+**Note:**  You will be asked to provide three inputs, an Ethereum node url, the proof file path, and the document file path. For the Ethereum node url, you can provision your own geth instance on your machine, or you can use a third-party node-as-a-service provider such as [Infura](https://infura.io).
--- src/index.js
+++ src/index.js
@@ -1,67 +1,68 @@
-const {Command, flags} = require('@oclif/command')
-const {pathToHash} = require('file-to-hash')
-const {MerkleTree} = require('merkletreejs')
+const { Command, flags } = require('@oclif/command')
+const { MerkleTree } = require('merkletreejs')
+const { pathToHash } = require('file-to-hash')
 const sha256 = require('crypto-js/sha256')
 const FileStream = require('fs')
-const {cli} = require('cli-ux')
+const { cli } = require('cli-ux')
 const Web3 = require('web3')
 
 class TwalaVerifyCommand extends Command {
   async run() {
-    const web3Provider = await cli.prompt('Node Provider (url)')
-    const web3 = new Web3(web3Provider)
-    const proofPath = await cli.prompt('Proof File (path)')
-    const documentPath = await cli.prompt('Document File (path)')
+    let web3Provider = await cli.prompt('Node Provider (url)')
+    let web3 = new Web3(web3Provider)
+    let proofPath = await cli.prompt('Proof File (path)')
+    let documentPath = await cli.prompt('Document File (path)')
     this.log()
     cli.action.start('generating proof hash')
-    await cli.wait(3000)
-    const initialProofHash = await pathToHash('sha256', proofPath)
-    const proofHash = web3.utils.keccak256(initialProofHash)
+    let initialProofHash = await pathToHash('sha256', proofPath)
+    let proofHash = web3.utils.keccak256(initialProofHash)
     cli.action.stop()
     cli.action.start('generating document hash')
-    await cli.wait(3000)
-    const initialDocumentHash = await pathToHash('sha256', documentPath)
-    const documentHash = web3.utils.keccak256(initialDocumentHash)
+    let initialDocumentHash = await pathToHash('sha256', documentPath)
+    let documentHash = web3.utils.keccak256(initialDocumentHash)
     cli.action.stop()
     cli.action.start('processing smart contracts')
-    await cli.wait(3000)
-    const proofHolderContractAbi = [{constant: true, inputs: [{name: '_hash', type: 'bytes32'}], name: 'retrieveProof', outputs: [{name: '', type: 'bytes32'}, {name: '', type: 'string'}, {name: '', type: 'string'}], payable: false, stateMutability: 'view', type: 'function'}, {constant: false, inputs: [{name: '_hash', type: 'bytes32'}, {name: '_root', type: 'string'}, {name: '_timestamp', type: 'string'}], name: 'recordProof', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function'}]
-    const proofHolderContractAddress = '0x07834b0C7B47892EafD0403461366292DB050e73'
-    const proofHolderContract = new web3.eth.Contract(proofHolderContractAbi, proofHolderContractAddress)
+    let proofHolderContractAbi = [{constant: true, inputs: [{name: '_hash', type: 'bytes32'}], name: 'retrieveProof', outputs: [{name: '', type: 'bytes32'}, {name: '', type: 'string'}, {name: '', type: 'string'}], payable: false, stateMutability: 'view', type: 'function'}, {constant: false, inputs: [{name: '_hash', type: 'bytes32'}, {name: '_root', type: 'string'}, {name: '_timestamp', type: 'string'}], name: 'recordProof', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function'}]
+    let proofHolderContractAddress = '0x07834b0C7B47892EafD0403461366292DB050e73'
+    let proofHolderContract = new web3.eth.Contract(proofHolderContractAbi, proofHolderContractAddress)
     cli.action.stop()
-    cli.action.start('searching Ethereum network')
-    const proofHolderProof = await proofHolderContract.methods.retrieveProof(proofHash).call()
-    const proof = {
+    cli.action.start('searching main Ethereum network')
+    let proofHolderProof = await proofHolderContract.methods.retrieveProof(proofHash).call()
+    let proof = {
       hash: proofHolderProof[0],
       root: proofHolderProof[1],
       timestamp: proofHolderProof[2],
     }
     cli.action.stop()
     cli.action.start('building merkle tree')
-    await cli.wait(5000)
-    const proofContent = await FileStream.readFileSync(proofPath, 'utf-8')
-    const proofContentArray = JSON.parse(web3.utils.hexToAscii(proofContent))
-    const merkleLeaves = proofContentArray.map(merkleLeaf => sha256(JSON.stringify(merkleLeaf)))
-    const merkleTree = new MerkleTree(merkleLeaves, sha256)
-    const merkleRoot = merkleTree.getRoot().toString('hex')
+    let proofContent = await FileStream.readFileSync(proofPath, 'utf-8')
+    let proofContentArray = JSON.parse(web3.utils.hexToAscii(proofContent))
+    let merkleLeaves = proofContentArray.map(merkleLeaf => sha256(JSON.stringify(merkleLeaf)))
+    let merkleTree = new MerkleTree(merkleLeaves, sha256)
+    let merkleRoot = merkleTree.getRoot().toString('hex')
     cli.action.stop()
     this.log()
-    if (proofHash === proof.hash) {
-      this.log(`Proof Hash: ${proofHash}`)
-      this.log(`Proof Timestamp: ${proof.timestamp}`)
-      if (merkleRoot === proof.root) {
-        this.log(`Merkle Root: ${merkleRoot}`)
-        for (let proofContent of proofContentArray) {
-          if (proofContent.hash === documentHash) {
-            this.log(`Document Hash: ${proofContent.hash}`)
-            this.log(`Document Timestamp: ${proofContent.timestamp}`)
+    let isDocumentFound = false
+    try {
+      if (proofHash === proof.hash) {
+        if (merkleRoot === proof.root) {
+          for (let proofContent of proofContentArray) {
+            if (proofContent.hash === documentHash) {
+              this.log(`Merkle Root: ${merkleRoot}`)
+              this.log(`Proof Hash: ${proofHash}`)
+              this.log(`Proof Timestamp: ${proof.timestamp}`)
+              this.log(`Document Hash: ${proofContent.hash}`)
+              this.log(`Document Timestamp: ${proofContent.timestamp}`)
+              isDocumentFound = true
+            }
           }
         }
-      } else {
-        this.log('proof invalid')
       }
-    } else {
-      this.log('proof invalid')
+      if (!isDocumentFound) {
+        this.log('document not found')
+      }
+    } catch (error) {
+      this.log('an error has occured')
     }
   }
 }
